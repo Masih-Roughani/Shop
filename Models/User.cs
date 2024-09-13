0ac839e66b6848f5cf84a592d8671932bfbc6ca7
Masih-Roughani <masihroughani2005@gmail.com>
2024-09-14 01:44:15 +0330

    Writing User class's properties

--- Models/User.cs
+++ Models/User.cs
@@ -2,5 +2,16 @@
 
 public class User
 {
-    
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public int Id { get; set; }
+    public double Money { get; set; }
+
+    public User(string username, string password, int id, double money)
+    {
+        Username = username;
+        Password = password;
+        Id = id;
+        Money = money;
+    }
 }
